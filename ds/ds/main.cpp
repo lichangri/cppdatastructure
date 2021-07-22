03d9409b433e9ab62aac601882936b0cbb85f7b3
李昌日 <lichangri@lichangrideMacBook-Pro.local>
2021-07-23 00:11:07 +0800

    se

---
+++ ds/ds/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ds
+//
+//  Created by 李昌日 on 2021/7/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
