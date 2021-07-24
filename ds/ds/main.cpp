4a494f39f988a1c33324f4286c8d390352ee682b
李昌日 <lichangri@lichangrideMacBook-Pro.local>
2021-07-24 20:25:44 +0800

    updateversion

--- ds/ds/main.cpp
+++ ds/ds/main.cpp
@@ -18,7 +18,7 @@ using namespace std;
 // 建立一个空表 + 数组
 // 应用什么场景
 // 为什么
-typedef struct shunxubiao *Listlcr ;// 这个放在哪里
+typedef struct shunxubiao *Listlcr ;// 这个放在哪里 定义的是别名 定义了之后就用别名
 struct shunxubiao{
     int data[MaxSize];//这是c里面的数组吗 c++需要注意什么 又别的用法吗 vector特性在哪里 填不满数据放哪里
     int lastindex;// c 和c++中结构一样的吗
@@ -36,55 +36,55 @@ Listlcr MakeEmpty(){
 
 // 表的增加元素 第i个位置增加元素 num
 // *号怎么用
-struct shunxubiao* addt(int i,int num,struct shunxubiao *Listlcr){
-    if(Listlcr->lastindex == MaxSize-1){
+Listlcr addt(int i,int num,Listlcr ptrl){
+    if(ptrl->lastindex == MaxSize-1){
         printf("顺序表已经满了");
-        return Listlcr;// 返回一个错误值 怎么返回 他说必须返回一个struct shunxubiao 类 不能返回NULL
+        return ptrl;// 返回一个错误值 怎么返回 他说必须返回一个struct shunxubiao 类 不能返回NULL
     }
     // 如果是从一个空的顺序表插入的话
-    if(Listlcr->lastindex == -1){
-        Listlcr->lastindex = 0;
-        Listlcr->data[0] = num;
-        return Listlcr;
+    if(ptrl->lastindex == -1){
+        ptrl->lastindex = 0;
+        ptrl->data[0] = num;
+        return ptrl;
     }
 
-    if(i < 0||i> Listlcr->lastindex+1){
+    if(i < 0||i> ptrl->lastindex+1){
         printf("输入位置错误");
-        return Listlcr;
+        return ptrl;
     }
     
     // for循环为什么没进去 第二个条件为正 就执行
-    for(int j = Listlcr->lastindex;j>=i-1;j--){
-        Listlcr->data[j+1] = Listlcr->data[j] ;
+    for(int j = ptrl->lastindex;j>=i-1;j--){
+        ptrl->data[j+1] = ptrl->data[j] ;
     }
-    Listlcr->data[i-1] = num;
-    Listlcr->lastindex = Listlcr->lastindex + 1;
-    return Listlcr;
+    ptrl->data[i-1] = num;
+    ptrl->lastindex = ptrl->lastindex + 1;
+    return ptrl;
 }
 
 //表 找元素 第i个元素
-int findi(int i,struct shunxubiao *Listlcr){
-    if(Listlcr->lastindex == 0){
+int findi(int i,Listlcr ptrl){
+    if(ptrl->lastindex == 0){
         printf("顺序表空的");
         return 0;//
     }
-    if(i < 0 || i> Listlcr->lastindex-1){
+    if(i < 0 || i> ptrl->lastindex-1){
         printf("输入位置错误");
         return 0;
     }
-    return Listlcr->data[i-1];
+    return ptrl->data[i-1];
 }
 
 // 表 找和某个元素一样的值的index 的第一个
 // 如果是很多个该怎么返回 因为屎了 int[maxsize] findv 报错
-int findv(int num,struct shunxubiao *Listlcr){
-    if(Listlcr->lastindex == 0){
+int findv(int num,Listlcr ptrl){
+    if(ptrl->lastindex == 0){
         printf("顺序表空的");
         return NULL;//
     }
-    int index;
-    for(int j =0; j<Listlcr->lastindex;j++){ // 循环至少跑一次是什么意思
-        if(Listlcr->data[j] == num){
+    int index=0;
+    for(int j =0; j<=ptrl->lastindex;j++){ // 循环至少跑一次是什么意思
+        if(ptrl->data[j] == num){
             index = j+1;
             return index;
         }
@@ -94,28 +94,30 @@ int findv(int num,struct shunxubiao *Listlcr){
     }// 这里经常报错是因为返回值嘛
 
 //删除操作 删除第i个数
-struct shunxubiao* deletev(int i,struct shunxubiao *Listlcr){
-    if(Listlcr->lastindex == 0){
+Listlcr deletev(int i,Listlcr ptrl){
+    if(ptrl->lastindex == 0){
         printf("表空");
-        return Listlcr;
+        return ptrl;
     }
-    if(i<0 || i>Listlcr->lastindex+1){
+    if(i<0 || i>ptrl->lastindex+1){
         printf("位置错误");
-        return Listlcr;
+        return ptrl;
     }
-    for(int j = i-1;j<Listlcr->lastindex-2;j++){
-        Listlcr->data[j] = Listlcr->data[j+1];
+    for(int j = i-1;j< ptrl->lastindex ;j++){
+        ptrl->data[j] = ptrl->data[j+1];
     }
-    Listlcr->lastindex = Listlcr->lastindex-1;
-    return Listlcr;
+    ptrl->lastindex = ptrl->lastindex-1;
+    return ptrl;
     
 }
 
-void printfbiao(struct shunxubiao *Listlcr){
-    int index = Listlcr->lastindex;
-    for(int j = 0;j>index;j++){
-       cout << Listlcr->data[j]<< endl;
+void printfTable(Listlcr ptrl){
+    int index = ptrl->lastindex;
+    printf("\n");
+    for(int j = 0;j<=index;j++){
+       printf("%d",ptrl->data[j]);
     }
+    printf("\n");
 }
 
 int main(int argc, const char * argv[]) {
@@ -123,20 +125,19 @@ int main(int argc, const char * argv[]) {
     // 创建一个新表 然后不断的通过软件
     Ptt = MakeEmpty();//函数 的调用；
     Ptt = addt(1,3,Ptt);//函数调用 *号的使用 一定要match
-    //printfbiao(Ptt); // 不能通过一个函数打印吗
-    printf("%d\n",Ptt->data[0]);
+    printfTable(Ptt);
     Ptt = addt(1,5,Ptt);//
-    printf("%d\n",Ptt->data[1]);
+    printfTable(Ptt);
     Ptt = addt(1,8,Ptt);//
-    printf("%d\n",Ptt->data[2]);//结果很奇怪 为什么呢 因为我的for循环写错了
+    printfTable(Ptt);//结果很奇怪 为什么呢 因为我的for循环写错了
     Ptt = addt(1,9,Ptt);//
-    printf("%d\n",Ptt->data[2]);
-    //删除操作
-    Ptt = deletev(1,Ptt);
-    printf("%d\n",Ptt->data[1]);//
+    printfTable(Ptt);    //删除操作
+    Ptt = deletev(3,Ptt);
+    printf("%d",Ptt->lastindex);
+    printfTable(Ptt);
     // 找到第i个数
     int k = findv(8,Ptt);
-    printf("这个值在表的 %d 个位置\n",k+1);//
+    printf("8这个值在表的 %d 个位置\n",k);//
     
     int ky = findi(1, Ptt);
     printf("第1个位置是 %d",ky);//
